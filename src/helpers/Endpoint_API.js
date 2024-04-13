f677f6ba57fb208346906719d5e8af943b43309a
aldair715 <79611817+aldair715@users.noreply.github.com>
2024-04-13 13:01:01 -0400

    Deleting key Endpoints
    Wer are deleting some information, push by error.

--- src/helpers/Endpoint_API.js
+++ src/helpers/Endpoint_API.js
@@ -1,10 +1,10 @@
 const SPOTIFY_COUNT = "https://accounts.spotify.com/api/token",
   SPOTIFY_ARTIST = "https://api.spotify.com/v1/search",
   LYRICS_API = "https://lyrist.vercel.app/api",
-  CLIEND_ID = "b7815b5f5cf3412cad4088b6b11359fa",
-  CLIENTE_SECRET = "acfa304096844a6588a708d8e34633f2",
+  CLIEND_ID = "",
+  CLIENTE_SECRET = "",
   BEARER =
-    "BQDBKJ5eo5jxbtpWjVOj7ryS84khybFpP_lTqzV7uV-T_m0cTfwvdn5BnBSKPxKgEb11";
+    "";
 export default {
   SPOTIFY_COUNT,
   SPOTIFY_ARTIST,
